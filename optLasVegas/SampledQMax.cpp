5a5d9cf1246a0e5f417f566d311a076e4cc209ac
bilal tayh <46609468+Bilal-Tayh@users.noreply.github.com>
2021-02-17 18:04:44 +0200

    Add files via upload

---
+++ optLasVegas/SampledQMax.cpp
@@ -0,0 +1,219 @@
+#include "SampledQMax.hpp"
+#include <iostream>
+#include <random>
+#include <math.h> 
+
+#include <vector>
+
+#include <bitset>
+//#include "unistd.h"
+#include <cstring>
+#include <algorithm>
+#include <memory.h>
+#include <stdlib.h>
+#include <chrono>
+#include <fstream>
+#include <assert.h>
+#include <time.h>
+#include <immintrin.h>
+#include <iomanip> 
+#include "RngFast.hpp"
+
+void SampledQMax::print(){
+	for (int i = 0; i < _actualsize; ++i)
+		std::cout << _A[i] << " ";
+	std::cout << std::endl;
+	std::cout << "_phi  = " << _phi << std::endl;
+}
+
+
+
+SampledQMax::SampledQMax(int q, float gamma){
+	_actualsize = q * (1+gamma);
+	_actualsizeMinusOne = _actualsize - 1;
+	_curIdx = _actualsize;
+	_A = (int*) malloc(sizeof(int) * _actualsize);
+	if (!_A)
+		exit(1);
+	_gamma = gamma;
+	_q = q;
+	_qMinusOne = q - 1;
+	_nminusq = _actualsize - q;
+	_phi = -1;
+    _delta = 0.001;
+    _alpha=0.83;
+    _psi = 2.0/3.0;
+    _k=ceil( ((_alpha*_gamma*(2+_gamma - _alpha*_gamma)) / (pow(_gamma -_alpha*_gamma,2))) * log(1/_delta));
+    _Z = (int)  ( (_k*(1+_gamma)) / (_alpha*_gamma) );
+    gen_arr();
+    rand_bits = _mm256_set_epi64x(gen_arr(), gen_arr(), gen_arr(), gen_arr());
+    counter=0;
+}
+
+
+
+void SampledQMax::insert(int v){
+	if (v < _phi){
+		return;
+	}
+	else{
+		_A[--_curIdx] = v;
+		if (_curIdx){
+			return;
+		}
+		else {
+			maintenance();
+		}
+	}
+}
+
+void SampledQMax::maintenance(){
+	_phi = findKthLargestAndPivot();
+}
+
+int* SampledQMax::largestQ(){
+	maintenance();
+	return _A + _nminusq;
+}
+
+inline void swap(int &x, int &y){
+	int z = x;
+	x = y;
+	y = z;
+}
+
+
+int SampledQMax::PartitionAroundPivot(int left, int right, int pivot_idx, int* nums) {
+	int pivot_value = nums[pivot_idx];
+	int new_pivot_idx = right;
+	swap(nums[pivot_idx], nums[right]);
+	for (int i = right-1; i >= left; --i) {
+		if (nums[i] > pivot_value) {
+			swap(nums[i], nums[--new_pivot_idx]);
+		}
+	}
+	swap(nums[right], nums[new_pivot_idx]);
+	return new_pivot_idx;
+}
+
+
+
+uint32_t SampledQMax::mm256_extract_epi32_var_indx(int i){   
+    __m128i indx = _mm_cvtsi32_si128(i);
+    __m256i val  = _mm256_permute2f128_si256(rand_bits, _mm256_castsi128_si256(indx),0);
+    return _mm_cvtsi128_si32(_mm256_castsi256_si128(val));
+}
+
+
+
+
+
+int SampledQMax::GenerateRandom(int max){
+//     return std::rand() % max; 
+    int indx = 0;
+    if(counter >= 8){
+        rand_bits =_mm256_set_epi64x(gen_arr(), gen_arr(), gen_arr(), gen_arr());
+        counter=0;
+    }
+    indx = mm256_extract_epi32_var_indx(counter)%max;
+    counter++;
+    return indx;
+}
+
+
+
+
+// if value dont exist in _A return -1, otherwise return the index of the value in _A
+int SampledQMax::findValueIndex(int value){
+    for(int i=0;i<_actualsize;i++){
+        if(_A[i]==value){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+// check if the conditions holds for the possible pivot "value"
+// return the pivot index in _A if it hold otherwise return -1
+int SampledQMax::checkPivot(int value){
+    int left = 0, right = _actualsizeMinusOne;
+    int pivot_idx = findValueIndex(value);
+    
+    
+    
+     if(pivot_idx==-1){
+        return -1;
+    }
+    int new_pivot_idx = PartitionAroundPivot(left, right, pivot_idx, _A);
+    
+
+    
+    if (new_pivot_idx <= int(_gamma*_q)) {
+        if(new_pivot_idx >= int(_gamma*_q*_psi)) {
+            return new_pivot_idx;
+        }
+    }
+    return -1;
+}
+
+
+
+
+int SampledQMax::findKthLargestAndPivot(){
+    int tries=2;
+    while(tries!=0){
+        
+        // p should contain the smallest _k values from the _Z sampled random values from _A
+       std::priority_queue <int> p;
+
+       for(int i=0;i<_k;i++){
+            int j=GenerateRandom(_actualsize);
+            p.push(_A[j]);
+        }
+        int top = p.top();
+        
+        for(int i=_k;i<_Z;i++){
+            int j=GenerateRandom(_actualsize);
+            if(top>_A[j]){
+                p.pop();
+                p.push(_A[j]); 
+                top = p.top();
+            }
+        }
+    
+        
+        
+        
+        // check if the conditions holds for the possible pivot B[Kth_minimal_idx]
+        int idx = checkPivot(top);
+        if(idx!=-1){
+            _curIdx = idx;
+            return _A[idx];
+        }
+
+        // if the conditions dont hold try sample Z elemnts from _A again...
+        tries--;
+    }
+    
+    // sampling didn't work twice so we are doing the old SampledQMax maintanance
+    int left = 0, right = _actualsizeMinusOne;
+	while (left <= right) {
+		int pivot_idx = left;
+		int new_pivot_idx = PartitionAroundPivot(left, right, pivot_idx, _A);
+		if (new_pivot_idx == _nminusq) {
+            _curIdx = new_pivot_idx;
+			return _A[new_pivot_idx];
+		} else if (new_pivot_idx > _nminusq) {
+			right = new_pivot_idx - 1;
+		} else {  // new_pivot_idx < _q - 1.
+			left = new_pivot_idx + 1;
+		}
+	}
+	
+}
+
+void SampledQMax::reset(){
+	_phi = -1;
+	_curIdx = _actualsize;
+}
---
+++ optLasVegas/test/graph5.cpp
@@ -0,0 +1,184 @@
+#include <cstdio>
+#include <cstdlib>
+#include <list>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <random>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/timeb.h>
+
+#include "../QmaxKV.hpp"
+#include "../HeapKV.hpp"
+#include "../SkiplistKV.hpp"
+#include "Utils.hpp"
+
+#define CLK_PER_SEC CLOCKS_PER_SEC
+#define CAIDA16_SIZE 152197437
+#define CAIDA18_SIZE 175880808
+#define UNIV1_SIZE 17323447
+
+using namespace std;
+
+void benchmark_psskiplist(int q, key** keys, val** vals, ofstream &ostream, string dataset, int numKeys) {
+  std::random_device _rd;
+  std::mt19937 _e2(_rd());
+  std::uniform_real_distribution<double> _dist(0,1);
+  key *elements = *keys;
+  val *weights = *vals;
+  struct timeb begintb, endtb;
+  clock_t begint, endt;
+  double time;
+  SkiplistKV sl(q);
+  begint = clock();
+  ftime(&begintb);
+  for (int i = 0; i < numKeys; i++) {
+    val priority = weights[i] / (1-_dist(_e2));
+    sl.add(pair<key, val>(elements[i], priority));
+  }
+  endt = clock();
+  ftime(&endtb);
+  time = ((double)(endt-begint))/CLK_PER_SEC;
+  ostream << dataset << ",SkipList," << numKeys << "," << q << ",," << time << endl;
+}
+
+void benchmark_psheap(int q, key** keys, val** vals, ofstream &ostream, string dataset, int numKeys) {
+  std::random_device _rd;
+  std::mt19937 _e2(_rd());
+  std::uniform_real_distribution<double> _dist(0,1);
+  key *elements = *keys;
+  val *weights = *vals;
+  struct timeb begintb, endtb;
+  clock_t begint, endt;
+  double time;
+  HeapKV heap(q);
+  begint = clock();
+  ftime(&begintb);
+  for (int i = 0; i < numKeys; i++) {
+    val priority = weights[i] / (1-_dist(_e2));
+    heap.add(pair<key,val>(elements[i], priority));
+  }
+  endt = clock();
+  ftime(&endtb);
+  time = ((double)(endt-begint))/CLK_PER_SEC;
+  ostream << dataset << ",Heap," << numKeys << "," << q << ",," << time << endl;
+}
+
+void benchmark_psqmax(int q, double gamma, key** keys, val** vals, ofstream &ostream, string dataset, int numKeys) {
+  std::random_device _rd;
+  std::mt19937 _e2(_rd());
+  std::uniform_real_distribution<double> _dist(0,1);
+  key *elements = *keys;
+  val *weights = *vals;
+  struct timeb begintb, endtb;
+  clock_t begint, endt;
+  double time;
+  QMaxKV qmax = QMaxKV(q, gamma);
+  begint = clock();
+  ftime(&begintb);
+  for (int i = 0; i < numKeys; i++) {
+    val priority = weights[i] / (1-_dist(_e2));
+    qmax.insert(elements[i], priority);
+  }
+  endt = clock();
+  ftime(&endtb);
+  time = ((double)(endt-begint))/CLK_PER_SEC;
+  ostream << time << endl;
+}
+
+void getKeysAndValsFromFile(string filename, vector<key*> &keys, vector<val*> &vals, int size) {
+  ifstream stream;
+  stream.open(filename, fstream::in | fstream::out | fstream::app);
+  if (!stream) {
+    throw invalid_argument("Could not open " + filename + " for reading.");
+  }
+
+  key* file_keys = (key*) malloc(sizeof(key) * size);
+  val* file_vals = (val*) malloc(sizeof(val) * size);
+
+  string line;
+  string len;
+  string id;
+  for (int i = 0; i < size; ++i){
+    getline(stream, line);
+    std::istringstream iss(line);
+    iss >> len;
+    iss >> id;
+    try {
+      file_keys[i] = stoull(id);
+      file_vals[i] = stoull(len);
+    } catch (const std::invalid_argument& ia) {
+      cerr << "Invalid argument: " << ia.what() << " at line " << i << endl;
+      cerr << len << " " << id << endl;;
+      --i;
+      exit(1);
+    }
+  }
+  keys.push_back(file_keys);
+  vals.push_back(file_vals);
+
+  stream.close();
+}
+
+int main() {
+  vector<ofstream*> streams;
+  vector<key*> keys;
+  vector<val*> vals;
+  vector<int> sizes;
+  vector<string> datasets;
+
+  ofstream univ1stream;
+  setupOutputFile("../results/ps_univ1.raw_res", univ1stream, false);
+  streams.push_back(&univ1stream);
+  getKeysAndValsFromFile("../datasets/UNIV1/mergedPktlen_Srcip", keys, vals, UNIV1_SIZE);
+  sizes.push_back(UNIV1_SIZE);
+  datasets.push_back("univ1");
+
+  ofstream caida16stream;
+  setupOutputFile("../results/ps_caida.raw_res", caida16stream, false);
+  streams.push_back(&caida16stream);
+  getKeysAndValsFromFile("../datasets/CAIDA16/mergedPktlen_Srcip", keys, vals, CAIDA16_SIZE);
+  sizes.push_back(CAIDA16_SIZE);
+  datasets.push_back("caida");
+
+  ofstream caida18stream;
+  setupOutputFile("../results/ps_caida18.raw_res", caida18stream, false);
+  streams.push_back(&caida18stream);
+  getKeysAndValsFromFile("../datasets/CAIDA18/mergedPktlen_Srcip", keys, vals, CAIDA18_SIZE);
+  sizes.push_back(CAIDA18_SIZE);
+  datasets.push_back("caida18");
+
+  list<unsigned int> qs = {10000, 100000, 1000000, 10000000};
+  for (int run = 0; run < 1; run++) {
+  for (unsigned q: qs) {
+    vector<key*>::iterator k_it = keys.begin();
+    vector<val*>::iterator v_it = vals.begin();
+    vector<int>::iterator s_it = sizes.begin();
+    vector<string>::iterator d_it = datasets.begin();
+    for (auto& stream : streams) {
+      key* k = *k_it;
+      val* v = *v_it;
+      int size = *s_it;
+      string dataset = *d_it;
+//       benchmark_psheap(q, &k, &v, *stream, dataset, size);
+//       benchmark_psskiplist(q, &k, &v, *stream, dataset, size);
+      list<double> gammas = {0.125, 0.25,0.5 , 1, 2,4};
+      for (double g : gammas) {
+        benchmark_psqmax(q, g, &k, &v, *stream, dataset, size);
+      }
+      ++k_it;
+      ++v_it;
+      ++s_it;
+      ++d_it;
+    }
+  }
+  }
+  univ1stream.close();
+  caida16stream.close();
+  caida18stream.close();
+  return 0;
+}
+
